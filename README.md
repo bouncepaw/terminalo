e0bd4af8983fc456f96cdde951e32b6d8e3cf52e
Timur Ismagilov <tims102rus@gmail.com>
2017-05-11 20:14:29 +0500

    check out terminalo.html

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # terminalo
+JS library to make useful terminal command lists.
+
+## Installation
+Clone this repo. `terminalo.js` is the only file you will need. Add it to your project.
+
+## Screenshots
+
+## Usage
+Use it like that:
+```html
+<div class="terminalo">
+  <div class="terminalo__line">git clone https://github.com/bouncepaw/terminalo.git</div>
+  <div class="terminalo__line">cd terminalo</div>
+</div>
+```
+
+You can use any tags.
+
+`.terminalo` is wrapper for lines. If you omit it, lines will be inline. `terminalo.js` starts after a page loads and then it adds copy buttons.
+
+For deeper explanation visit `terminalo.html`.
---
+++ terminalo.js
@@ -0,0 +1,45 @@
+var terminaloCopy = "^C"
+
+let terminals = document.getElementsByClassName("terminalo")
+let lines = document.getElementsByClassName("terminalo__line")
+
+let clipboard = new Clipboard(".terminalo__button")
+
+let styles = document.createElement("style")
+styles.innerHTML = `
+.terminalo__line {
+  display: inline-block;
+  background-color: black;
+  color: white;
+}
+.terminalo__cmd {
+  background-color: black;
+  font-family: monospace;
+}
+.terminalo__button {
+  padding: 0;
+  background-color: black;
+}
+.terminalo .terminalo__line {
+  display: block;
+}
+`
+
+document.getElementById("insert-here").appendChild(styles)
+
+for (var i = 0; i < lines.length; i++) {
+  let line = lines[i]
+  let command = line.textContent
+  line.innerHTML = ""
+
+  let button = document.createElement("button")
+  button.className = `terminalo__button terminalo__button_${i}`
+  button.innerHTML = terminaloCopy
+
+  let cmd = document.createElement("span")
+  cmd.className = `terminalo__cmd terminalo__cmd_${i}`
+  cmd.innerHTML = command
+
+  line.appendChild(button)
+  line.appendChild(cmd)
+}
